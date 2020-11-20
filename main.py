bad06f0a8fd1d251feec8caaac2da9bdd67305e4
Arjun <arjunshekhawatarjun@gmail.com>
2020-11-20 10:06:07 +0530

    Added logic for joining a class

--- main.py
+++ main.py
@@ -1,34 +1,113 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
+from datetime import date, datetime
+from schedule import classes
 
-PATH = '/home/kali/Downloads/geckodriver'
-driver = webdriver.Firefox(executable_path=PATH)
+
+def get_day():
+    return datetime.now().weekday()
+
+
+def get_time():
+    return datetime.now().time()
+
+
+opt = webdriver.ChromeOptions()
+opt.add_argument("--disable-infobars")
+opt.add_argument("start-maximized")
+opt.add_argument("--disable-extensions")
+
+# opt.add_argument("--start-maximized")
+# Pass the argument 1 to allow and 2 to block permissions
+opt.add_experimental_option("prefs", {
+    "profile.default_content_setting_values.media_stream_mic": 1,
+    "profile.default_content_setting_values.media_stream_camera": 1,
+    "profile.default_content_setting_values.geolocation": 1,
+    "profile.default_content_setting_values.notifications": 1
+},)
+opt.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+driver = webdriver.Chrome(options=opt)
+
 url = "https://teams.microsoft.com/"
 with open('creds.txt', 'r') as creds:
     creds = creds.read().split()
     email = creds[0]
     pas = creds[1]
 
-driver.get(url)
-time.sleep(3)
-driver.find_element_by_name('loginfmt').send_keys(email)
-time.sleep(2)
-driver.find_element_by_id('idSIButton9').click()
-time.sleep(2)
-driver.find_element_by_name('passwd').send_keys(pas)
-time.sleep(2)
-driver.find_element_by_id('idSIButton9').click()
-time.sleep(2)
-driver.find_element_by_id('idBtn_Back').click()
-time.sleep(2)
-driver.find_element_by_class_name('use-app-lnk').click()
-time.sleep(5)
-driver.find_element_by_xpath("//*[contains(text(),'Azad')]").click()
-n = 3
 
-try:
-    driver.find_element_by_class_name(
-        'ts-sym ts-btn ts-btn-primary inset-border icons-call-jump-in ts-calling-join-button app-title-bar-button app-icons-fill-hover call-jump-ink').click()
-except:
-    print('No join link')
+def login():
+    global driver
+    # enter email and click on login
+    driver.find_element(By.NAME, 'loginfmt').send_keys(email)
+    time.sleep(5)
+    driver.find_element(By.ID, 'idSIButton9').click()
+    time.sleep(5)
+
+    # enter password and click on login
+    driver.find_element(By.NAME, 'passwd').send_keys(pas)
+    time.sleep(5)
+    driver.find_element(By.ID, 'idSIButton9').click()
+    time.sleep(5)
+
+    # clicks on button where it asks to remeber the user
+    # clicks on no to have the consistency in each instance of login
+    driver.find_element(By.ID, 'idBtn_Back').click()
+    time.sleep(5)
+
+    # proceeds onto website when asked to continue on app
+    driver.find_element(By.CLASS_NAME, 'use-app-lnk').click()
+
+
+def join(clas):
+    global driver
+
+    time.sleep(5)
+    driver.find_element(By.XPATH, f"//*[contains(text(),'{clas}')]").click()
+    time.sleep(5)
+    try:
+        driver.find_element(
+            By.XPATH, '//*[@title="Join call with video"]').click()
+    except:
+        print('No join link')
+
+    time.sleep(10)
+
+    camera = driver.find_element(
+        By.XPATH, '//toggle-button[@text = "Enable video"]')
+    if camera.get_attribute('title') == 'Turn camera off':
+        camera.click()
+    time.sleep(5)
+
+    microphone = driver.find_element(By.ID, 'preJoinAudioButton')
+    if microphone.get_attribute('title') == 'Mute microphone':
+        microphone.click()
+    time.sleep(5)
+
+    driver.find_element(
+        By.XPATH, '//*[@id="page-content-wrapper"]/div[1]/div/calling-pre-join-screen/div/div/div[2]/div[1]/div[2]/div/div/section/div[1]/div/div/button').click()
+    time.sleep(3600)
+    leave_class()
+
+
+def leave_class():
+    global driver
+
+
+def main():
+    global driver
+    driver.get(url)
+    time.sleep(5)
+
+    if 'login' in driver.current_url:
+        login()
+    day = get_day()
+    clas = classes(day)
+    for c in clas:
+        print(c[0])
+        join(c[0])
+
+
+main()
---
+++ schedule.py
@@ -0,0 +1,46 @@
+schedule = {
+    0: [
+        [
+            "BTN 457 | Adv. Virology",
+            11,
+            12
+        ]
+    ],
+    1: [
+        [
+            "Biomolecular NMR",
+            10,
+            11
+        ]
+    ],
+    2: [
+        [
+            "Biomolecular NMR",
+            11,
+            12
+        ]
+    ],
+    3: [
+        [
+            "BTN 457 | Adv. Virology",
+            10,
+            11
+        ]
+    ],
+    4: [
+        [
+            "Biomolecular NMR",
+            10,
+            11
+        ],
+        [
+            "BTN 457 | Adv. Virology",
+            11,
+            12
+        ]
+    ]
+}
+
+
+def classes(day):
+    return schedule[day]
